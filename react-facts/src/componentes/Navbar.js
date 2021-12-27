a76f98fe821280206b521c39d266135b5667f096
Luis Cruz <luisccc99@gmail.com>
2021-12-27 13:34:18 -0700

    finish structure for first project

--- react-facts/src/componentes/Navbar.js
+++ react-facts/src/componentes/Navbar.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import logo from '../logo.svg'
-console.log(logo);
+import logo from '../images/logo.svg'
 
 const Navbar = () => {
     return (
         <nav id="navbar">
-            <div id="title-page">
+            <div id="title-logo">
                 <img src={logo} alt="React Logo" />
                 <h3>ReactFacts</h3>
             </div>
-            <h4>React Course - Project 1</h4>
+            <h4 className='margin-right'>React Course - Project 1</h4>
         </nav>
     );
 };
