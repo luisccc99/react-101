0631957bea59f0639f688cd5f9c092eec61dde86
Luis Cruz <luisccc99@gmail.com>
2022-01-07 15:58:59 -0700

    add card commponent

--- airbnb-experiences-clone/src/components/Hero.js
+++ airbnb-experiences-clone/src/components/Hero.js
@@ -1,11 +1,13 @@
 import HeroImage from "./HeroImage";
 import HeroDescription from "./HeroDescription";
+import ExperienceCard from "./ExperienceCard";
 
 const Hero =  () => {
     return (
         <section className="hero">
             <HeroImage />
             <HeroDescription />
+            <ExperienceCard />
         </section>
     );
 };
--- airbnb-experiences-clone/src/components/HeroDescription.js
+++ airbnb-experiences-clone/src/components/HeroDescription.js
@@ -1,10 +1,13 @@
 const HeroDescription = () => {
-    return (
-        <div>
-            <h1>Online Experiences</h1>
-            <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>Online Experiences</h1>
+      <p>
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </div>
+  );
+};
 
 export default HeroDescription;
